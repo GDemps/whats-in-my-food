a97d2086e276ca85f06feaab11c403da62895703
Grant Dempsey <CryptoDemps@gmail.com>
2020-12-15 14:40:29 -0700

    Trying to push to new repo

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,13 +5,14 @@ class FoodsController < ApplicationController
       f.params["api_key"] = ENV["API_KEY"]
     end
 
-    response = conn.get("/fdc/v1/foods/search") do |req|
-      req.params[:q] = query
-    end
-    # https://api.nal.usda.gov/fdc/v1/foods/search?api_key=fl6k6gMJqzehZDs1qP9uq2dDi2gaK9Fj2stPDIUx&query=Cheddar%20Cheese
+    # response = conn.get("/fdc/v1/foods/search?") do |req|
+    #   req.params[:q] = query
+    # end
+    response = conn.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=fl6k6gMJqzehZDs1qP9uq2dDi2gaK9Fj2stPDIUx&query=#{query}")
 
     json = JSON.parse(response.body, symbolize_names: true)
-  
     @foods = json[:foods]
+    require "pry"
+    binding.pry
   end
 end
